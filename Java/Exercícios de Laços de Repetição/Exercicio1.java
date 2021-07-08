272e50332639bc2be2509e27b1491f767587f83a
Suellen Castro <86411679+SuCastro@users.noreply.github.com>
2021-07-08 12:27:59 -0300

    Update Exercicio1.java

--- Java/Exercícios de Laços de Repetição/Exercicio1.java
+++ Java/Exercícios de Laços de Repetição/Exercicio1.java
@@ -1,10 +1,11 @@
-package LaçosDeRepetição;
+/* 1 - Informar todos os nÃºmeros de 1000 a 1999 que divididos quando por 11 obtemos resto = 5.*/
+
+package LaÃ§osDeRepetiÃ§Ã£o;
 
 public class Exercicio1 {
 
 	public static void main(String[] args) 
-	{// 1 - Informar todos os números de 1000 a 1999 que divididos quando por 11 obtemos resto = 5.
-		
+	{
 		for (int i=1000; i<2000; i++)
 		{
 			if (i%11==5) 
