656e4f4ebb3143733200a7fafa87395b007127e7
Suellen Castro <86411679+SuCastro@users.noreply.github.com>
2021-07-08 12:28:57 -0300

    Update Exercicio2.java

--- Java/Exercícios de Laços de Repetição/Exercicio2.java
+++ Java/Exercícios de Laços de Repetição/Exercicio2.java
@@ -1,19 +1,20 @@
-package LaÁosDeRepetiÁ„o;
+/* 1 - Ler 10 n√∫meros e imprimir quantos s√£o pares e quantos s√£o √≠mpares. */
+
+package La√ßosDeRepeti√ß√£o;
 
 import java.util.Scanner;
 
 public class Exercicio2 {
 
 	public static void main(String[] args) 
-	{// 1 - Ler 10 n˙meros e imprimir quantos s„o pares e quantos s„o Ìmpares.
-		
+	{	
 		int n=0, par= 0, impar=0;
 		
 		Scanner ler = new Scanner(System.in);
 		
 		for (int i=0; i<10; i++) {
 			
-			System.out.println("Entre com dez n˙meros: ");
+			System.out.println("Entre com dez n√∫meros: ");
 			n = ler.nextInt();
 		
 			if(n%2==0) 
@@ -26,6 +27,6 @@ public class Exercicio2 {
 				impar++;
 			}
 		}	
-		System.out.printf("%d n˙meros s„o pares e %d n˙meros s„o impares",par, impar);
+		System.out.printf("%d n√∫meros s√£o pares e %d n√∫meros s√£o impares",par, impar);
 	}
 }
