00c2f85d5349b34ed917dd0eff46d20f634e0139
Suellen Castro <86411679+SuCastro@users.noreply.github.com>
2021-07-08 12:31:28 -0300

    Update Exercicio5.java

--- Java/Exercícios de Laços de Repetição/Exercicio5.java
+++ Java/Exercícios de Laços de Repetição/Exercicio5.java
@@ -1,28 +1,27 @@
-package LaçosDeRepetição;
+/*5 - 	Crie um programa que leia um nÃºmero do teclado atÃ© que encontre um 
+	nÃºmero igual a zero. No final, mostre a soma dos nÃºmeros digitados. */
+
+package LaÃ§osDeRepetiÃ§Ã£o;
 
 import java.util.Scanner;
 
 public class Exercicio5 {
 
 	public static void main(String[] args) 
-	{/*
-		5 - Crie um programa que leia um número do teclado até que encontre um 
-		número igual a zero. No final, mostre a soma dos números digitados.
-		(DO...WHILE) */
-
+	{
 		int n=0, soma=0;
 		
 		Scanner tec = new Scanner(System.in);
 		
 		do {
-			System.out.print("Informa quantos números quiser até que a entrada seja 0: ");
+			System.out.print("Informa quantos nÃºmeros quiser atÃ© que a entrada seja 0: ");
 			n = tec.nextInt();
 			
 			soma = soma+n;
 
 		}while (n > 0);
 		if(n == 0) {
-			System.out.printf("O número 0 foi encontrado e o total da soma dos numeros digitados foi: %d", soma);
+			System.out.printf("O nÃºmero 0 foi encontrado e o total da soma dos numeros digitados foi: %d", soma);
 			tec.close();
 		}
 		else {
