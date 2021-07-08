a84e0050583f0471a034271dd7bba8bb55bd9a2b
Suellen Castro <86411679+SuCastro@users.noreply.github.com>
2021-07-08 02:54:03 -0300

    Add files via upload

---
+++ Java/Exercícios de Laços de Repetição/Exercicio2.java
@@ -0,0 +1,31 @@
+package LaçosDeRepetição;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) 
+	{// 1 - Ler 10 números e imprimir quantos são pares e quantos são ímpares.
+		
+		int n=0, par= 0, impar=0;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		for (int i=0; i<10; i++) {
+			
+			System.out.println("Entre com dez números: ");
+			n = ler.nextInt();
+		
+			if(n%2==0) 
+			{
+				par++;
+			}
+		
+			else
+			{
+				impar++;
+			}
+		}	
+		System.out.printf("%d números são pares e %d números são impares",par, impar);
+	}
+}
